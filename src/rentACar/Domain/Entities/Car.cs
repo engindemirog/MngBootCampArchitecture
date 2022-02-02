203e5ba48c92c64d639056fec629aa0ab79b1542
Engin Demiroğ <engindemirog@gmail.com>
2022-02-02 13:48:20 +0300

    Day 3 Created Models

--- src/rentACar/Domain/Entities/Car.cs
+++ src/rentACar/Domain/Entities/Car.cs
@@ -1,4 +1,5 @@
 ﻿using Core.Persistence.Repositories;
+using Domain.Enums;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,19 +15,21 @@ namespace Domain.Entities
 
         }
 
-        public Car(int id, int colorId, int modelId, string plate, short modelYear):this()
+        public Car(int id, int colorId, int modelId, string plate, short modelYear,CarState carState):this()
         {
             Id = id;
             ColorId = colorId;
             ModelId = modelId;
             Plate = plate;
             ModelYear = modelYear;
+            CarState = carState;
         }
 
         public int ColorId { get; set; }
         public int ModelId { get; set; }
         public string Plate { get; set; }
         public short ModelYear { get; set; }
+        public CarState CarState { get; set; }
 
         public virtual Color Color{ get; set; }
         public virtual Model Model { get; set; }
--- src/rentACar/Persistence/Contexts/BaseDbContext.cs
+++ src/rentACar/Persistence/Contexts/BaseDbContext.cs
@@ -15,12 +15,17 @@ namespace Persistence
 
         public DbSet<Brand> Brands { get; set; }
         public DbSet<Model> Models { get; set; }
+        public DbSet<Car> Cars { get; set; }
+        public DbSet<Color> Colors { get; set; }
+        public DbSet<Transmission> Transmissions { get; set; }
+        public DbSet<Fuel> Fuels { get; set; }
+
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            if (!optionsBuilder.IsConfigured) { 
-                base.OnConfiguring(optionsBuilder.UseSqlServer(Configuration.GetConnectionString("RentACarConnectionString")));
-            }
+            //if (!optionsBuilder.IsConfigured) { 
+            //    base.OnConfiguring(optionsBuilder.UseSqlServer(Configuration.GetConnectionString("RentACarConnectionString")));
+            //}
             
         }
 
@@ -34,6 +39,43 @@ namespace Persistence
                 b.Property(p=>p.Id).HasColumnName("Id");
                 b.Property(p => p.Name).HasColumnName("Name");
                 b.HasMany(p=>p.Models);
+            });
+
+            modelBuilder.Entity<Color>(b =>
+            {
+                b.ToTable("Colors").HasKey(k => k.Id);
+                b.Property(p => p.Id).HasColumnName("Id");
+                b.Property(p => p.Name).HasColumnName("Name");
+                b.HasMany(p => p.Cars);
+            });
+
+            modelBuilder.Entity<Fuel>(b =>
+            {
+                b.ToTable("Fuels").HasKey(k => k.Id);
+                b.Property(p => p.Id).HasColumnName("Id");
+                b.Property(p => p.Name).HasColumnName("Name");
+                b.HasMany(p => p.Models);
+            });
+
+            modelBuilder.Entity<Transmission>(b =>
+            {
+                b.ToTable("Transmissions").HasKey(k => k.Id);
+                b.Property(p => p.Id).HasColumnName("Id");
+                b.Property(p => p.Name).HasColumnName("Name");
+                b.HasMany(p => p.Models);
+            });
+
+            modelBuilder.Entity<Car>(b =>
+            {
+                b.ToTable("Cars").HasKey(k => k.Id);
+                b.Property(p => p.Id).HasColumnName("Id");
+                b.Property(p => p.ModelYear).HasColumnName("ModelYear");
+                b.Property(p => p.Plate).HasColumnName("Plate");
+                b.Property(p => p.ColorId).HasColumnName("ColorId");
+                b.Property(p => p.ModelId).HasColumnName("ModelId");
+                b.Property(p => p.CarState).HasColumnName("State");
+                b.HasOne(p => p.Color);
+                b.HasOne(p => p.Model);
             });
         }
     }
