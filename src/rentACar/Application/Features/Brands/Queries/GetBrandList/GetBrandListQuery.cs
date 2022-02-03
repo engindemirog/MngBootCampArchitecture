57ae47e9b0d7c5157a77645df1c22abad02f2b1e
Engin Demiroğ <engindemirog@gmail.com>
2022-02-03 10:22:02 +0300

    Day 4 Implemented CQRS Query Handler

---
+++ src/rentACar/Application/Features/Brands/Queries/GetBrandList/GetBrandListQuery.cs
@@ -0,0 +1,40 @@
+﻿using Application.Features.Brands.Models;
+using Application.Services.Repositories;
+using AutoMapper;
+using Core.Application.Requests;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.Brands.Queries.GetBrandList
+{
+    public class GetBrandListQuery:IRequest<BrandListModel>
+    {
+        public PageRequest PageRequest { get; set; }
+
+        public class GetBrandListQueryHandler : IRequestHandler<GetBrandListQuery, BrandListModel>
+        {
+            IBrandRepository _brandRepository;
+            IMapper _mapper;
+
+            public GetBrandListQueryHandler(IBrandRepository brandRepository, IMapper mapper)
+            {
+                _brandRepository = brandRepository;
+                _mapper = mapper;
+            }
+
+            public async Task<BrandListModel> Handle(GetBrandListQuery request, CancellationToken cancellationToken)
+            {
+                var brands = await _brandRepository.GetListAsync(
+                    index:request.PageRequest.Page,
+                    size:request.PageRequest.PageSize);
+
+                var mappedBrands = _mapper.Map<BrandListModel>(brands);
+                return mappedBrands;
+            }
+        }
+    }
+}
--- src/rentACar/WebAPI/Controllers/BrandsController.cs
+++ src/rentACar/WebAPI/Controllers/BrandsController.cs
@@ -1,4 +1,6 @@
 ﻿using Application.Features.Brands.Commands.CreateBrand;
+using Application.Features.Brands.Queries.GetBrandList;
+using Core.Application.Requests;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -13,6 +15,15 @@ namespace WebAPI.Controllers
         {
             var result = await Mediator.Send(createBrandCommand);
             return Created("", result);
+        }
+
+        [HttpGet("getall")]
+        public async Task<IActionResult> GetAll([FromQuery] PageRequest pageRequest)
+        {
+            var query = new GetBrandListQuery();
+            query.PageRequest = pageRequest;
+            var result = await Mediator.Send(query);
+            return Ok(result);
         }
 
     }
